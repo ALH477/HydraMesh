e6323e009291465435126b3ac466cdfd504bd5c9
Asher <146795551+ALH477@users.noreply.github.com>
2025-08-25 02:17:31 -0700

    Create dcf.py

---
+++ python/dcf.py
@@ -0,0 +1,8 @@
+# God said to Moses, “I AM WHO I AM. This is what you are to say to the Israelites: ‘I AM has sent me to you.’” (Exodus 3:14)
+# Copyright (C) 2025 DeMoD LLC
+# This file is part of DeMoD Communications Framework.
+# Licensed under GPL-3.0 (see LICENSE in repo root).
+from dcf.cli import cli
+
+if __name__ == "__main__":
+    cli()
